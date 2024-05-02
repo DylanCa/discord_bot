601e4ad304032853b8fce43820a09fc0d4b1055a
DylanCa <dylan.cattelan@gmail.com>
2024-05-02 21:58:39 +0200

    Added VoiceEventHandler trait to TrackErrorNotifier

--- src/main.rs
+++ src/main.rs
@@ -132,6 +132,26 @@ async fn leave(
     ctx.say(format!("Leaving <#{}>!", channel_id)).await?;
     Ok(())
 }
+
+struct TrackErrorNotifier;
+
+#[async_trait]
+impl VoiceEventHandler for TrackErrorNotifier {
+    async fn act(&self, ctx: &EventContext<'_>) -> Option<Event> {
+        if let EventContext::Track(track_list) = ctx {
+            for (state, handle) in *track_list {
+                println!(
+                    "Track {:?} encountered an error: {:?}",
+                    handle.uuid(),
+                    state.playing
+                );
+            }
+        }
+
+        None
+    }
+}
+
 #[tokio::main]
 async fn main() {
     dotenv().ok();
