8e977743df391a57ef5318fdea5ebe00fd499583
DylanCa <dylan.cattelan@gmail.com>
2024-05-02 21:56:25 +0200

    Added age method to know user account age

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::str::FromStr;
+use dotenv::dotenv;
+use poise::{CreateReply, serenity_prelude as serenity};
+use poise::serenity_prelude::GuildId;
+use serenity::async_trait;
+use songbird::events::{Event, EventContext, EventHandler as VoiceEventHandler, TrackEvent};
+use songbird::SerenityInit;
+use reqwest::Client as HttpClient;
+use serenity::all::{CreateEmbed, Timestamp};
+use serenity::prelude::TypeMapKey;
+use serde::Deserialize;
+use songbird::input::cached::LengthHint::Time;
+
+struct HttpKey;
+
+impl TypeMapKey for HttpKey {
+    type Value = HttpClient;
+}
+struct Data {} // User data, which is stored and accessible in all command invocations
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+/// Displays your or another user's account creation date
+#[poise::command(slash_command, prefix_command)]
+async fn age(
+    ctx: Context<'_>,
+    #[description = "Selected user"] user: Option<serenity::User>,
+) -> Result<(), Error> {
+    let u = user.as_ref().unwrap_or_else(|| ctx.author());
+    let response = format!("{}'s account was created at {}", u.name, u.created_at());
+    ctx.say(response).await?;
+    Ok(())
+}
 #[tokio::main]
 async fn main() {
     dotenv().ok();
